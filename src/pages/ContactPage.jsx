eb8d486c191a259785006491dd014b3ad4f50847
Uniquecon <akinwunmiolusegun277@gmail.com>
2024-12-21 20:32:30 +0100

    built the html template for the contact page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,6 +1,26 @@
 const ContactPage = () => {
   return (
     <>
+      <main className="flex flex-col lg:flex-row">
+        <div>
+          <h1></h1>
+          <p></p>
+          <section>
+            <div>
+              <h3></h3>
+              <p></p>
+            </div>
+          </section>
+        </div>
+        <div>
+          <h1></h1>
+          <p></p>
+          <form>
+            <input type="text" name="" id="" />
+            <button type="submit"></button>
+          </form>
+        </div>
+      </main>
     </>
   );
 };
