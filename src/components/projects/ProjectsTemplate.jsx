853ed5ebd3565d0700518ab3a7eee93af5012f04
Uniquecon <akinwunmiolusegun277@gmail.com>
2024-12-28 10:11:25 +0100

    updated the project

--- src/components/projects/ProjectsTemplate.jsx
+++ src/components/projects/ProjectsTemplate.jsx
@@ -5,7 +5,7 @@ import StackSpan from "../common/StackSpan";
 
 const ProjectsTemplate = ({ projectsArray }) => {
   return (
-    <main className="grid place-content-center md:grid-cols-2 lg:grid-cols-3 md:gap-x-8 lg:gap-x-6 w-[78.5%] md:w-4/5 lg:w-10/12">
+    <main className="grid place-content-center md:grid-cols-2 lg:grid-cols-3 md:gap-x-8 lg:gap-x-6 w-[77.5%] md:w-4/5 lg:w-10/12">
       {projectsArray.map(({ id, project_image, project_topic, project_text, project_github, project_link, project_stack }) => {
         return (
           <div key={id} className="flex flex-col bg-grey-semidark shadow-xl transition-all hover:-translate-y-2 active:translate-y-2 disabled:animate-pulse disabled:cursor-not-allowed disabled:opacity-50 disabled:translate-y-0 rounded-2xl overflow-hidden">
