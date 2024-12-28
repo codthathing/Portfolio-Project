a441850f1502f19214c180ab2d7d6a6868eb76b6
Uniquecon <akinwunmiolusegun277@gmail.com>
2024-12-28 09:56:24 +0100

    updated the project

--- src/components/common/StackSpan.jsx
+++ src/components/common/StackSpan.jsx
@@ -9,7 +9,7 @@ const StackSpan = ({ stackArray, className }) => {
         return (
           <span key={id} className={`px-3 py-1 md:px-6 md:py-2 lg:px-2 lg:py-1 rounded-full flex items-center gap-x-1 md:gap-x-2 lg:gap-x-1 ${className}`}>
             {stack_name.toLowerCase() === "tailwind css" || stack_name.toLowerCase() === "netlify" || stack_name.toLowerCase() === "vercel" || stack_name.toLowerCase() === "git" ? <stackDetails.icon /> : <FontAwesomeIcon icon={stackDetails.icon} />}
-            <p className="font-semibold font-Roboto">{stackDetails.text}</p>
+            { stackDetails.text && <p className="font-semibold font-Roboto">{stackDetails.text}</p> }
           </span>
         );
       })}
