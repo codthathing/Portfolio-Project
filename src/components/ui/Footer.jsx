fe52c51ef7d4671c9124b4c153d5091b96f466c9
Uniquecon <akinwunmiolusegun277@gmail.com>
2025-01-06 22:45:31 +0100

    Updated the website performance and accessibility and also added some meta tags for optimization

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -6,9 +6,9 @@ import { useEffect, useState } from "react";
 
 const Footer = () => {
   const socialsPages = [
-    { id: 0, icon: faXTwitter, socialsLink: "https://x.com/codthathing" },
-    { id: 1, icon: faLinkedin, socialsLink: "https://linkedin.com/in/codthathing" },
-    { id: 2, icon: faGithub, socialsLink: "https://github.com/codthathing" },
+    { id: 0, text: "Twitter", icon: faXTwitter, socialsLink: "https://x.com/codthathing" },
+    { id: 1, text: "Linkedin", icon: faLinkedin, socialsLink: "https://linkedin.com/in/codthathing" },
+    { id: 2, text: "Github", icon: faGithub, socialsLink: "https://github.com/codthathing" },
   ];
   const location = useLocation().pathname;
   const [showInformation, setShowInformation] = useState({ div: true, section: true });
@@ -34,7 +34,7 @@ const Footer = () => {
   return (
     <footer className={`relative p-4 md:py-8 md:px-10 lg:py-5 lg:px-16`}>
       <div className={`${location !== "/" ? "invisible" : ""} md:visible flex items-center gap-x-5 md:gap-x-10 lg:gap-x-7`}>
-        {socialsPages.map(({ id, icon, socialsLink }) => <a key={id} target="_blank" href={socialsLink}><FontAwesomeIcon icon={icon} className="text-gray-400 text-base md:text-2xl lg:text-xl transition-all ease-linear duration-200 hover:scale-125" /></a>)}
+        {socialsPages.map(({ id, text, icon, socialsLink }) => <a key={id} target="_blank" aria-label={text} href={socialsLink}><FontAwesomeIcon icon={icon} className="text-gray-400 text-base md:text-2xl lg:text-xl transition-all ease-linear duration-200 hover:scale-125" /></a>)}
       </div>
       <div className={`absolute right-4 bottom-4 md:right-10 md:bottom-8 lg:right-16 lg:bottom-5 max-w-[60%] md:max-w-[65%] text-white bg-grey-dark/50 backdrop-blur-sm rounded-full shadow-lg border border-grey-dark/20 p-2 md:p-4 lg:p-3 ${showInformation.div ? "visible" : "invisible"} flex items-center gap-x-2 md:gap-x-4 lg:gap-x-3`}>
         <FontAwesomeIcon icon={faRobot} onClick={() => setShowInformation(prevState => ({ ...prevState, section: !prevState.section }))} className="text-sm md:text-2xl lg:text-xl cursor-pointer" />
