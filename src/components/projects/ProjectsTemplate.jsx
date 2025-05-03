71704dcfd77f35c5c16d0bdac6348a3dad6fb990
Uniquecon <akinwunmiolusegun277@gmail.com>
2025-05-03 14:38:23 +0100

    fix: updated the select property of the product template and the contact page

--- src/components/projects/ProjectsTemplate.jsx
+++ src/components/projects/ProjectsTemplate.jsx
@@ -6,7 +6,7 @@ const ProjectsTemplate = ({ projectsArray }) => {
     <main className="grid place-content-center md:grid-cols-2 lg:grid-cols-3 md:gap-x-8 lg:gap-x-6 w-[77.5%] md:w-4/5 lg:w-10/12">
       {projectsArray.map(({ id, project_image, project_topic, project_text, project_github, project_link, project_stack }) => {
         return (
-          <div key={id} className="flex flex-col bg-grey-semidark shadow-xl rounded-2xl overflow-hidden">
+          <div key={id} className="flex flex-col select-none bg-grey-semidark shadow-xl rounded-2xl overflow-hidden">
             <img src={project_image} loading="lazy" fetchPriority="high" alt={project_topic.toUpperCase()} className="w-full h-36 md:h-56 lg:h-52" />
             <section className="flex flex-col flex-1 p-4 md:p-6 lg:p-4">
               <h1 className="capitalize font-bold text-base md:text-2xl lg:text-xl text-white font-Yantramanav">{project_topic}</h1>
