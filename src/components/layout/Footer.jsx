6646674e4d96174e1b76fda167b35e8420135b10
Uniquecon <akinwunmiolusegun277@gmail.com>
2025-05-14 00:29:07 +0100

    Updated the text shown below

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -14,9 +14,9 @@ const Footer = () => {
   const [informationDetails, setInformationDetails] = useState({
     array: [
       { id: 0, text: "Welcome to my portfolio!" },
-      { id: 1, text: "On mobile: Explore using the navigation menu at the top-right" },
+      { id: 1, text: "On mobile: Explore using the icon at the top-right" },
       { id: 2, text: "Click on the robot to expand/collapse" },
-      { id: 3, text: "Click on the icon at the top-left to navigate to the front page" },
+      { id: 3, text: "Navigate to home page using the icon at the top-left" },
       { id: 4, text: "Click on the email address to send an email directly" },
     ],
     text: "Welcome to my portfolio!",
