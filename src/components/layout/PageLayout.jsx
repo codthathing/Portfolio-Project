ae063bf26c27a67c3b2fda35473b77859ae4691b
Uniquecon <akinwunmiolusegun277@gmail.com>
2025-05-13 13:28:08 +0100

    updated the home background

--- src/components/layout/PageLayout.jsx
+++ src/components/layout/PageLayout.jsx
@@ -13,7 +13,7 @@ const PageLayout = () => {
   }, []);
 
   return (
-    <PageDiv initial={{ x: initialX }} animate={{ x: 0, transition: { duration: 0.5 } }} className={"relative flex-col h-screen overflow-hidden"}>
+    <PageDiv initial={{ x: initialX }} animate={{ x: 0, transition: { duration: 0.5 } }} className={"relative flex-col h-[100dvh] overflow-hidden"}>
       <header className="relative py-4 md:py-8 md:px-12 lg:py-5 lg:px-16 2xl:px-24 flex justify-between items-center">
         <img src={portfolio_logo} className="ml-4 md:m-0 w-10 md:w-16 lg:w-12 cursor-pointer select-none transition-all ease-linear duration-200 hover:animate-pulse" onClick={() => navigate("/")} alt="PORTFOLIO LOGO" />
         <PageNavigation />
