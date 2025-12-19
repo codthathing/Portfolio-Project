a6d3389c7176c4f8b459171ffe86afaa264f06f4
Akinwunmi <akinwunmiolusegun277@gmail.com>
2025-12-19 21:53:10 +0100

    Nothing much

--- src/components/projects/ProjectsTemplate.jsx
+++ src/components/projects/ProjectsTemplate.jsx
@@ -1,4 +1,4 @@
-import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
+import { FaBookOpen, FaExternalLinkAlt, FaGithub } from "react-icons/fa";
 import StackSpan from "../common/StackSpan";
 import { motion, AnimatePresence } from "framer-motion";
 import { usePagePosition } from "../../hooks/usePagePosition";
@@ -20,15 +20,18 @@ const ProjectsTemplate = ({ projectsArray, keyId, animation }) => {
           return (
             <div key={id} className="flex flex-col select-none bg-grey-semidark shadow-xl rounded-2xl overflow-hidden">
               <img src={project_image} loading="lazy" fetchpriority="high" alt={project_topic.toUpperCase()} className="w-full h-36 md:h-56 lg:h-52" />
-              <section className="flex flex-col flex-1 p-4 md:p-6 lg:p-4">
-                <h1 className="capitalize font-bold text-base md:text-2xl lg:text-xl text-white font-Yantramanav">{project_topic}</h1>
-                <div className="my-4 md:my-6 lg:my-4 flex-1">
+              <section className="flex flex-col flex-1 p-4 md:p-6 lg:px-4 lg:py-6">
+                <div className="flex justify-between items-center text-white">
+                  <h1 className="capitalize font-bold text-base md:text-2xl lg:text-xl font-Yantramanav">{project_topic}</h1>
+                  <FaBookOpen className="cursor-pointer" />
+                </div>
+                <div className="my-4 md:my-6 lg:my-6 flex-1 overflow-hidden">
                   <p className="text-xs md:text-lg lg:text-base line-clamp-4 md:line-clamp-3 text-grey-textdark font-Roboto">{project_text}</p>
                 </div>
                 <div className="flex flex-wrap gap-x-2 gap-y-1 md:gap-x-4 md:gap-y-2 lg:gap-x-2 lg:gap-y-1 text-[8px] md:text-lg lg:text-sm">
                   <StackSpan stackArray={project_stack} className={"border-[0.5px] md:border border-grey-borderdark text-white"} />
                 </div>
-                <div className="flex items-center gap-x-8 md:gap-x-12 lg:gap-x-10 self-end mt-4 md:mt-6 lg:mt-4 text-xs md:text-lg lg:text-base text-white">
+                <div className="flex items-center gap-x-8 md:gap-x-12 lg:gap-x-10 text-xs md:text-lg lg:text-base mt-4 md:mt-8 lg:mt-6 text-white self-end">
                   <a href={`https://github.com/codthathing/${project_github}`} target="_blank" rel="noopener noreferrer" className="cursor-pointer">
                     <FaGithub />
                   </a>
