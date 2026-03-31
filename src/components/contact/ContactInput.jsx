05a8ab372bf5049ac092d00fdf42dd307c7a3884
Akinwunmi <akinwunmiolusegun277@gmail.com>
2026-03-31 13:06:59 +0100

    fix: reactivated the email vendor and the style of the input in the contact page

--- src/components/contact/ContactInput.jsx
+++ src/components/contact/ContactInput.jsx
@@ -1,5 +1,5 @@
 const ContactInput = ({ type, name, value, onChange, placeholder, inputStyle }) => {
-  return <input type={type} name={name} autoComplete="on" value={value} onChange={onChange} placeholder={placeholder} required className={`text-gray-400 text-[8px] md:text-sm lg:text-xs py-2 lg:py-3 font-Roboto w-full shadow-inset border-b border-neutral-700 bg-transparent placeholder:${inputStyle} tracking-wider focus:border-neutral-500 focus:outline-none`} />;
+  return <input type={type} name={name} value={value} onChange={onChange} placeholder={placeholder} required className={`font-Roboto shadow-inset bg-neutral-900 text-gray-400 text-[8px] md:text-sm lg:text-xs px-1 py-2 lg:py-3 outline-none w-full border-b border-neutral-700 tracking-wider focus:border-neutral-500 ${inputStyle}`} />;
 };
 
 export default ContactInput;
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -13,7 +13,7 @@ const ContactPage = () => {
     { id: 1, topic: "Email", text: "akinwunmiolusegun277@gmail.com", type: "link" },
   ];
 
-  const [showPopUp, setShowPopUp] = useState(false);
+  const [showPopUp, setShowPopUp] = useState({ show: false, text: "Message Sent!" });
 
   const [contactInput, setContactInput] = useState({ name: "", email: "", title: "", message: "" });
   const handleContactInput = (e) => {
@@ -27,23 +27,28 @@ const ContactPage = () => {
   const handleEmailSubmit = () => {
     if (contactInput.name && contactInput.email && contactInput.title && contactInput.message) {
       emailjs.send("service_f904uuc", "template_xp3618s", { name: contactInput.name, email: contactInput.email, title: contactInput.title, message: contactInput.message }, "WlCoMP40HZDFpDPbX").then(
-        (response) => console.log("SUCCESS!", response.status, response.text),
-        (error) => console.log("FAILED...", error)
+        (response) => {
+          console.log("SUCCESS!", response.status, response.text);
+          setContactInput({ name: "", email: "", title: "", message: "" });
+          setShowPopUp({ text: "Message Sent!", show: true });
+        },
+        (error) => {
+          console.log("FAILED...", error);
+          setShowPopUp({ text: "Message Failed!", show: true });
+        },
       );
-      setContactInput({ name: "", email: "", title: "", message: "" });
-      setShowPopUp(true);
     } else {
-      setContactInputStyle({ name: `text-${contactInput.name ? "gray" : "red"}-400`, email: `text-${contactInput.email ? "gray" : "red"}-400`, title: `text-${contactInput.title ? "gray" : "red"}-400`, message: `text-${contactInput.message ? "gray" : "red"}-400` });
+      setContactInputStyle({ name: `placeholder:text-${contactInput.name ? "gray" : "red"}-400`, email: `placeholder:text-${contactInput.email ? "gray" : "red"}-400`, title: `placeholder:text-${contactInput.title ? "gray" : "red"}-400`, message: `placeholder:text-${contactInput.message ? "gray" : "red"}-400` });
     }
   };
 
   return (
     <>
-      {showPopUp && (
+      {showPopUp.show && (
         <motion.main initial={{ x: 500 }} animate={{ x: 0, transition: { duration: 1, damping: 9, type: "spring", ease: "easeInOut" } }} className="fixed w-full top-0 right-0 md:w-fit md:top-6 md:right-6 lg:top-4 lg:right-10">
           <div className="relative py-6 md:py-8 md:px-20 lg:py-6 bg-neutral-800/50 backdrop-blur-sm text-gray-400 md:rounded-tl-3xl md:rounded-br-3xl">
-            <p className="text-center text-xs md:text-xl lg:text-base">Message Sent!</p>
-            <FaX className="absolute top-3 right-3 md:top-4 md:right-4 lg:top-2 lg:right-2 text-white text-xs md:text-base cursor-pointer" onClick={() => setShowPopUp(false)} />
+            <p className="text-center text-xs md:text-xl lg:text-base">{showPopUp.text}</p>
+            <FaX className="absolute top-3 right-3 md:top-4 md:right-4 lg:top-2 lg:right-2 text-white text-xs md:text-base cursor-pointer" onClick={() => setShowPopUp({ ...showPopUp, show: false })} />
           </div>
         </motion.main>
       )}
@@ -71,7 +76,7 @@ const ContactPage = () => {
                 <ContactInput type={"text"} name={"name"} value={contactInput.name} onChange={handleContactInput} placeholder={"Your Name"} inputStyle={contactInputStyle.name} />
                 <ContactInput type={"email"} name={"email"} value={contactInput.email} onChange={handleContactInput} placeholder={"Your Email"} inputStyle={contactInputStyle.email} />
                 <ContactInput type={"text"} name={"title"} value={contactInput.title} onChange={handleContactInput} placeholder={"Email Title"} inputStyle={contactInputStyle.title} />
-                <textarea name="message" value={contactInput.message} onChange={handleContactInput} placeholder="Message" required className={`text-gray-400 text-[8px] md:text-sm lg:text-xs py-2 lg:py-3 font-Roboto w-full border-b border-neutral-700 bg-transparent placeholder:${contactInputStyle.message} resize-none tracking-wider focus:border-neutral-500 focus:outline-none`} id="" />
+                <textarea name="message" value={contactInput.message} onChange={handleContactInput} placeholder="Message" required className={`text-gray-400 text-[8px] md:text-sm lg:text-xs py-2 lg:py-3 font-Roboto w-full border-b border-neutral-700 bg-transparent ${contactInputStyle.message} resize-none tracking-wider focus:border-neutral-500 focus:outline-none`} id="" />
               </div>
               <button type="button" onClick={handleEmailSubmit} className="font-Yantramanav mt-6 md:mt-10 lg:mt-8 py-2 px-4 md:px-5 md:py-3 text-xs md:text-lg lg:text-base rounded-lg text-white bg-zinc-800 bg-opacity-30 uppercase tracking-wider shadow-2xl transition-all hover:-translate-y-1 active:translate-y-1 disabled:animate-pulse disabled:cursor-not-allowed disabled:opacity-50 disabled:translate-y-0">
                 SEND MESSAGE
